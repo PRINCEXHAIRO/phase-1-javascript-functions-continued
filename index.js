292e8f3e65e069a53dfabf0ca99d94f5a5fffdb9
Hairo Barnard <Hairo.barnard@gmail.com>
2023-11-02 12:13:13 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+};
+
+console.log(saturdayFun("bathe my dog"))
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`    
+
+};
+
+console.log(mondayWork("work from home"))
+
+function wrapAdjective(wrap = '*') {
+    return function(adjective = 'special') {
+    
+    return `You are ${wrap}${adjective}${wrap}!`
+    
+    };
+};
+console.log(wrapAdjective());
